652428c5777e9c74cec8cccd6ce0684e58ba408a
Simone <92912149+SimoneZumerle@users.noreply.github.com>
2023-01-16 11:31:33 +0100

    Update registration.php
    inseriti pulsanti submit e fix completo al codice

--- registration.php
+++ registration.php
@@ -1,37 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Register</title>
+        <title>FIVEHOUSE | Registration</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
-        <link rel="stylesheet" href="style/styles.css">
-        <meta charset="utf-8"/>
-        <title>
-            FIVEHOUSE | Registration
-        </title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open%20Sans"/>
-        <link rel="stylesheet" type="text/css" href="./style/styles.css"/>
-        <header style=color:white>
-        </header>
+        <style>
+        <?php include './style/styles.css'; ?>
+</style>
     </head>
-    <body style=background-color:#2A4A50>
-        <form method="post" action="/php/login.php" class="form">
+    <body style=background-color:#2b3031>
+        <form style=class:form>
             <fieldset style=class:fieldset>
                 <legend style=class:legend>
-                    <h1 class=text> Register </h1>
+                    <h1 style=class:h1> Register </h1>
                 </legend>
+</br>
                 <input type="text" id="first_name" placeholder="first name" name="name" maxlength="50" required>
         <pre></pre>
-                <input type="text" id="last_name" placeholder="last name" name="surname" maxlength="50" required>
+                <input type="text" id="last_name" placeholder="last name" name="surname" maxlength="50" style=class:input-type required>
         <pre></pre>
                 <input type="text" id="username" placeholder="Username" name="username" maxlength="50" required>
         <pre></pre>
                 <input type="email" id="email" placeholder="email" name="mail" maxlength="50" required>
         <pre></pre>
                 <input type="password" id="password" placeholder="Password" name="password" required>
-        <pre>
-        </pre>
-                <p>already a partner?</br><a href=login.php>click me!</a></p>
+        <pre></pre>
+                <input type="submit" value="Send Request" />
+        <pre></pre>
             </fieldset>
+                <p style=color:#ffffff>already a partner?<a href=login.php>click me!</a></p>
         </form>
     </body>
 </html>
