9ecd134b9037f65b60b1b47dbe288a4a4ad00bfc
Simone <92912149+SimoneZumerle@users.noreply.github.com>
2023-02-27 10:45:50 +0100

    Update login.php

--- pagina/login.php
+++ pagina/login.php
@@ -1,30 +1,79 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FIVEHOUSE | Login</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
-        <style>
-        <?php include './style/styles.css'; ?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <title>FIVEHOUSE | Opzioni di accesso</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/pricing/">
+
+    
+<style>
+    .row{
+        background-color:linear-gradient(#e66465, #9198e5);
+    }
+    .form-control{
+        flex:row;
+    }
 </style>
-    </head>
-    <body class="body">
-    <script src="terranovajs.js"></script>
-        <form style=class:form>
-            <fieldset style=class:fieldset>
-                <legend style=class:legend>
-                    <h1> Login </h1>
-                </legend>
-</br>
-                <input type="email" style=margin-top:15% id="email" placeholder="email" name="mail" maxlength="50" required>
-        <pre></pre>
-                <input type="password" id="password" placeholder="Password" name="password" required>
-                <pre></pre>
-                <input onmouseover="change_bg(this)" onmouseout="change_bg_back(this)" type="submit" value="Login">
-        <pre></pre>
-        <p><a href=registration.php class="links">Need an account?</a>
-        </fieldset>
-        </form>
-        <script>
-</script>
-    </body>
+    
+
+<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Section: Design Block -->
+<body>
+<?php if(isset($_REQUEST['username']) and isset($_REQUEST['password'])) {
+    echo "<h1>Benvenuto, " . $_REQUEST['username'] . "</h1>";}?>
+
+<section class="section">
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+      <div class="container">
+        <div class="row gx-lg-5 align-items-center">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="card">
+              <div class="card-body py-5 px-md-5">
+                <form>
+                    <legend>
+                        <div class="col-lg-6 mb-5 mb-lg-0">
+                            <h1 class="my-5 display-3 fw-bold ls-tight"> Form di Login
+                            </h1>
+                          </div>
+                    </legend>
+                  <!-- 2 column grid layout with text inputs for the first and last names -->
+  
+                  <!-- Email input -->
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form3Example3" class="form-control" />
+                    <label class="form-label" for="form3Example3">Email address</label>
+
+  
+                  <!-- Password input -->
+
+                    <input type="password" id="form3Example4" class="form-control" />
+                    <label class="form-label" for="form3Example4">Password</label>
+                  </div>
+  
+                  <!-- Checkbox -->
+  
+                  <!-- Submit button -->
+                  <button type="submit" class="btn btn-primary btn-block mb-4">
+                    Log in
+                  </button>
+                  <p><a href=register.php class="links">Need an account?</a>
+  
+                  <!-- Register buttons -->
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
 </html>
