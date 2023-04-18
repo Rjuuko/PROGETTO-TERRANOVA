fab742534b01e3e13c030b2e5b7b94d59623a5d3
Kaelid <92912149+SimoneZumerle@users.noreply.github.com>
2023-04-18 08:46:30 +0200

    inserito css

--- pagina/login/verification.php
+++ pagina/login/verification.php
@@ -18,29 +18,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <title>FIVEHOUSE | Login</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/pricing/">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .diverso{
+            border:1.5px solid rgba(0,0,0,0.25);
+            margin-top:10vw;
+            position:flex;
+            display: flex;
+            justify-content: center;
+            background-color:rgba(0,0,0,10%);
+        }
+    </style>
 </head>
 <body>
-    <div>
-        <h1> Verifica l'email </h1>
-        <p> Controlla la posta elettronica e incolla il codice ricevuto nel box qui sotto </p>
-        <?php 
-            if(isset($_REQUEST['e'])){
-                if($_REQUEST['e'] == 1){
-                ?>
-                    <span> Codice inserito non corretto </span>
-                <?php
+<div class="b-example-divider"></div>
+<div class=diverso>
+<div class="px-4 pt-5 my-5 text-center border-bottom"></div>
+    <center>
+        <div class="col">
+            <strong>
+            <h1 class="my-5 display-3 fw-bold ls-tight">Verifica l'email</h1>
+            </strong>
+        </div>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+            <div class=form-group>
+
+            <p style=font-size:large style=margin-top:2vw> Controlla la posta elettronica e incolla il codice ricevuto nel box qui sotto </p>
+            <?php 
+                if(isset($_REQUEST['e'])){
+                    if($_REQUEST['e'] == 1){
+                    ?>
+                        <div class=alert>
+                        <span class="closebtn"><strong> Codice inserito non corretto </strong></span>
+                    <?php
+                    }
                 }
-            }
-            
-        ?>
-        <form action="" method="POST">
-            <input type="text" name="conf" id="conf">
-            <input type="submit" value="conferma">
-        </form>
+                
+            ?>
+            <form action="" method="POST">
+                <input class=form-control style=width:30vw;margin-top:2vw;height:2.5vw type="text" name="conf" id="conf">
+                <input class="btn btn-primary btn-block mb-4" style=margin-top:2vw;height:2.5vw;width:6vw;align-text:center type="submit" value="conferma">
+            </form>
+        </div>
+        <div class="overflow-hidden" style="max-height: 30vh;">
+        </div>
     </div>
+    <div class="b-example-divider"></div>
+    </center>
+</div>
+            
+            
 </body>
 </html>
