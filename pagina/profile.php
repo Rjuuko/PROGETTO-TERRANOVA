cc7c434098113bb5c13f96c8b394343307503cc4
Kaelid <92912149+SimoneZumerle@users.noreply.github.com>
2023-04-20 20:03:08 +0200

    css inserito

--- pagina/profile.php
+++ pagina/profile.php
@@ -52,46 +52,90 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/pricing/">
+    
     <title>Document</title>
-</head>
-<body>
-    <h1> Profilo Personale </h1>
-    <h3> Dettagli personali <h3>
-    <form method="POST" action="">
-    <?php  
-    for($i = 0; $i < count($data); $i++){
-
-        ?>
-         <h4> <?= $data[$i] ?><h4> 
-
-         
-        <?php
-        if($inputType[$i] == "sel"){
-            ?>
-            <select name="Nazione" class="form-control" id="Nazione" >
-            <?php require("./addData/country_selector.php");?>
-            </select> <br>
-            <?php
-        }else if($inputType[$i] == "sel1"){
-            ?>
-            <select id="Provincia" class="form-control" name="Provincia" >
-            <?php require("./addData/selettore_provincia.php"); ?>
-            </select> <br>
-
-            <?php
-        }else if($inputType[$i] == "email"){
-            ?>
-            <input type="Email" id="<?= $data[$i]?>" class="form-control" name="Email" value="<?= $row[$data[$i]]?>" />
-            <?php
-        }else{
-            ?>
-            <input type="text" id="<?= $data[$i]?>" class="form-control" name="<?= $data[$i]?>" value="<?= $row[$data[$i]]?>" />
-            <?php
+    <style>
+        #main{
+            display:grid;
+            justify-content:center;
         }
-    }
-   
-    ?>
-    <input type="submit">
-    </form>
+        input{
+            max-width:20vw;
+        }
+        select{
+            max-width:20vw;
+        }
+        #card{
+            display:flex;
+            justify-content:center;
+        }
+        input{
+            margin:0 auto;
+            display:block;
+        }
+        #container {
+            display: flex;
+            justify-content: center; /* Can be changed in the live sample */
+        }
+    </style>
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body id="main" style="background-color: hsl(0, 0%, 96%);">
+<section class="section" >
+    <div class="px-4 py-5 px-md-5 text-center text-lg-center">
+      <div class="container align-items-center">
+        <div class="row gx-lg-5 align-items-center">
+
+            <div class="container" id="container">
+                <div class="card" style=width:47vw;align-content:center>
+                    <div id="main">
+                <h1> Profilo Personale </h1><br>
+                <h3> Dettagli personali <h3><br>
+                <form method="POST" action="" style=justify-content:center>
+                <?php  
+                for($i = 0; $i < count($data); $i++){
+
+                    ?>
+                    <h4> <?= $data[$i] ?><h4> 
+
+                    
+                    <?php
+                    if($inputType[$i] == "sel"){
+                        ?>
+                        <select name="Nazione" class="form-control" id="Nazione" style=justify-self:center >
+                        <?php require("./addData/country_selector.php");?>
+                        </select> <br>
+                        <?php
+                    }else if($inputType[$i] == "sel1"){
+                        ?>
+                        <select id="Provincia" class="form-control" name="Provincia" >
+                        <?php require("./addData/selettore_provincia.php"); ?>
+                        </select> <br>
+
+                        <?php
+                    }else if($inputType[$i] == "email"){
+                        ?>
+                        <input type="Email" id="<?= $data[$i]?>" class="form-control" name="Email" value="<?= $row[$data[$i]]?> " /><br>
+                        <?php
+                    }else{
+                        ?>
+                        <input type="text" class="form-control" id="<?= $data[$i]?>" name="<?= $data[$i]?>" value="<?= $row[$data[$i]]?>" />
+                        <?php
+                    }
+                }
+            
+                ?>
+                <br>
+                <div style=justify-content:center><input type="submit" class="btn btn-primary btn-block mb-4">
+                </form>
+                    </div>
+        </div>
+    </div>                
 </body>
 </html>
